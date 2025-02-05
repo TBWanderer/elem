4e6f3a59f1189f1241a484a95e0af24fa131e1d5
TBWanderer <evgenypodyryaka@yandex.ru>
2025-02-05 21:49:38 +0300

    Fixed cond

--- src/conditions.rs
+++ src/conditions.rs
@@ -6,11 +6,15 @@ pub fn leval(expr: Value) -> Value {
 }
 
 pub fn lcond(states: Value) -> Value {
-    if states.is_list() && states.len() > 0 && states.get(0).is_list() && states.get(1).is_list() {
+    if states.is_list() {
         let states: Vec<Value> = states.into();
         for state in states.clone() {
-            if leval(state.get(0).clone()) == num!(1) {
-                return leval(state.get(1).clone());
+            if state.len() == 2 {
+                if leval(state.get(0).clone()) == num!(1) {
+                    return leval(state.get(1).clone());
+                }
+            } else {
+                panic!("cond state error!")
             }
         }
     } else {

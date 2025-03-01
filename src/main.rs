2c37ada73a4a54d6ce57d11d1d0ddf8c45ee0491
TBWanderer <evgenypodyryaka@yandex.ru>
2025-03-02 00:39:13 +0300

    Added way to read code from file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
+use clap::{arg, command};
 use elem::runtime;
 
 fn main() {
+    let matches = command!()
+        .arg(arg!(-f --file <FILE>).required(false))
+        .get_matches();
+
     let mut elem_runtime = runtime::Runtime::new();
 
+    if let Some(path) = matches.get_one::<String>("file") {
+        let code = runtime::utils::io::read_file(&path);
+        elem_runtime.run(&code);
+    }
+
     loop {
-        let line = runtime::utils::input("lithium@elem >> ");
+        let line = runtime::utils::io::input("lithium@elem >> ");
         elem_runtime.run(&line);
     }
 }

68eacf8f04b0c269c3f4fedaf17ef72955dcce15
TBWanderer <evgenypodyryaka@yandex.ru>
2025-03-01 20:38:18 +0300

    Added reading in loop

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,36 @@
 use elem::prelude::*;
 
+fn input(prompt: &str) -> String {
+    use std::io::{self, BufRead, Write};
+    print!("{} {}", "[>]", prompt);
+    match io::stdout().flush() {
+        Ok(_) => (),
+        Err(e) => panic!(),
+    };
+
+    match io::stdin()
+        .lock()
+        .lines()
+        .next()
+        .unwrap()
+        .map(|x| x.trim_end().to_owned())
+    {
+        Ok(input) => input,
+        Err(err) => {
+            panic!()
+        }
+    }
+}
+
 fn main() {
     let mut scopes = init_scopes();
-    let line = "(+ (1 2))"; // It says that key not found. I didn't fix it yet
-    let tokens = tokenize(line.to_string());
-    let values = parse(tokens);
+    loop {
+        let line = input("lithium@elem >> ");
+        let tokens = tokenize(line.to_string());
+        let values = parse(tokens);
 
-    for value in values {
-        println!("{}", eval(value, &mut scopes));
+        for value in values {
+            println!("{}", eval(value, &mut scopes));
+        }
     }
 }

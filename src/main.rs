96d1a957c0a5a4a6f2763ecffb96836aa2fdd5bc
TBWanderer <evgenypodyryaka@yandex.ru>
2025-03-01 20:20:11 +0300

    Mini test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-use elem::list;
+use elem::prelude::*;
 
 fn main() {
-    let list = list!["Hello", "world", "!"];
-    println!("{list}")
+    let mut scopes = init_scopes();
+    let line = "(+ (1 2))";
+    let tokens = tokenize(line.to_string());
+    let values = parse(tokens);
+
+    for value in values {
+        println!("{}", eval(value, &mut scopes));
+    }
 }

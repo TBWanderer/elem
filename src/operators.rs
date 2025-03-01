7198aceafc0282f5f29ea6979371aeb5db99b1e1
TBWanderer <evgenypodyryaka@yandex.ru>
2025-03-01 18:30:41 +0300

    Added let, set, eval

--- src/operators.rs
+++ src/operators.rs
@@ -1,6 +1,8 @@
+use super::scopes::Scopes;
 use super::value::*;
+use crate::nil;
 
-pub fn ladd(list: Value) -> Value {
+pub fn ladd(list: Value, _scopes: &mut Scopes) -> Value {
     let mut sum = 0;
     let v: Vec<Value> = list.into();
 
@@ -15,7 +17,7 @@ pub fn ladd(list: Value) -> Value {
     Value::Number(sum)
 }
 
-pub fn lmul(list: Value) -> Value {
+pub fn lmul(list: Value, _scopes: &mut Scopes) -> Value {
     let mut res = 1;
     let v: Vec<Value> = list.into();
 
@@ -30,7 +32,7 @@ pub fn lmul(list: Value) -> Value {
     Value::Number(res)
 }
 
-pub fn leq(list: Value) -> Value {
+pub fn leq(list: Value, _scopes: &mut Scopes) -> Value {
     let v: Vec<Value> = list.into();
     if v.len() != 2 {
         panic!("Too many arguments!");
@@ -43,7 +45,7 @@ pub fn leq(list: Value) -> Value {
     }
 }
 
-pub fn lgt(list: Value) -> Value {
+pub fn lgt(list: Value, _scopes: &mut Scopes) -> Value {
     let v: Vec<Value> = list.into();
     if v.len() != 2 {
         panic!("Too many arguments!");
@@ -60,7 +62,7 @@ pub fn lgt(list: Value) -> Value {
     }
 }
 
-pub fn llt(list: Value) -> Value {
+pub fn llt(list: Value, _scopes: &mut Scopes) -> Value {
     let v: Vec<Value> = list.into();
     if v.len() != 2 {
         panic!("Too many arguments!");
@@ -77,7 +79,7 @@ pub fn llt(list: Value) -> Value {
     }
 }
 
-pub fn ldiv(list: Value) -> Value {
+pub fn ldiv(list: Value, _scopes: &mut Scopes) -> Value {
     let v: Vec<Value> = list.into();
     if v.len() != 2 {
         panic!("Too many arguments!");
@@ -93,7 +95,7 @@ pub fn ldiv(list: Value) -> Value {
     }
 }
 
-pub fn lsub(list: Value) -> Value {
+pub fn lsub(list: Value, _scopes: &mut Scopes) -> Value {
     let v: Vec<Value> = list.into();
     if v.len() != 2 {
         panic!("Too many arguments!");
@@ -103,5 +105,76 @@ pub fn lsub(list: Value) -> Value {
         Value::Number(a_num - b_num)
     } else {
         panic!("Arguments must be numbers")
+    }
+}
+
+pub fn lset(list: Value, scopes: &mut Scopes) -> Value {
+    use super::scopes;
+
+    let v: Vec<Value> = list.into();
+    if v.len() != 2 {
+        panic!("Not 2 arguments!")
+    }
+
+    if let Value::Name(k) = &v[0] {
+        scopes::change(scopes.to_vec(), k.to_string(), leval(v[1].clone(), scopes))
+    } else {
+        panic!("1st arg isn't name type")
+    }
+    nil!()
+}
+
+pub fn llet(list: Value, scopes: &mut Scopes) -> Value {
+    use super::scopes;
+
+    let args: Vec<Value> = list.into();
+    if args.len() != 2 {
+        panic!("Not 2 arguments!")
+    }
+
+    scopes::new(scopes);
+
+    let sets: Vec<Value> = args[0].clone().into();
+    for i in 0..sets.len() {
+        lset(sets[i].clone(), scopes);
+    }
+
+    let result = leval(args[1].clone(), scopes);
+
+    scopes::pop(scopes.to_vec());
+
+    result
+}
+
+pub fn leval(expr: Value, scopes: &mut Scopes) -> Value {
+    use super::scopes;
+    use crate::pair;
+    match expr {
+        Value::Nil => expr,
+        Value::Number(_) => expr,
+        Value::Macros(_) => expr,
+        Value::Function(_) => expr,
+        Value::Name(name) => scopes::get(scopes.to_vec(), name),
+        Value::Pair(action, args) => match *action {
+            Value::Macros(macros) => macros((*args).clone(), scopes),
+            Value::Function(function) => {
+                if !args.is_list() {
+                    panic!("This is not list: {}", args)
+                } else {
+                    let args_vec: Vec<Value> = (*args).clone().into();
+                    let evaluated_args = Value::from(
+                        args_vec
+                            .into_iter()
+                            .map(|x| leval(x, scopes))
+                            .collect::<Vec<Value>>(),
+                    );
+                    function(evaluated_args, scopes)
+                }
+            }
+            _ => panic!(
+                "Can't eval this list: {}",
+                pair!((*action).clone(), (*args).clone())
+            ),
+        },
     }
 }

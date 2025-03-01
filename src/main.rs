c75e139b7441324ba01fc82338f3c1d402931741
TBWanderer <evgenypodyryaka@yandex.ru>
2025-03-01 20:26:13 +0300

    Comments

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use elem::prelude::*;
 
 fn main() {
     let mut scopes = init_scopes();
-    let line = "(+ (1 2))";
+    let line = "(+ (1 2))"; // It says that key not found. I didn't fix it yet
     let tokens = tokenize(line.to_string());
     let values = parse(tokens);
 

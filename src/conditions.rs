18dd89a4469ae8e73aa87c4adbc424f288ae8544
TBWanderer <evgenypodyryaka@yandex.ru>
2025-03-01 18:32:01 +0300

    Fixed conditions

--- src/conditions.rs
+++ src/conditions.rs
@@ -1,17 +1,14 @@
+use super::operators::leval;
 use super::value::*;
 use crate::num;
 
-pub fn leval(expr: Value) -> Value {
-    expr
-}
-
-pub fn lcond(states: Value) -> Value {
+pub fn lcond(states: Value, scopes: &mut super::scopes::Scopes) -> Value {
     if states.is_list() {
         let states: Vec<Value> = states.into();
         for state in states.clone() {
             if state.len() == 2 {
-                if leval(state.get(0).clone()) == num!(1) {
-                    return leval(state.get(1).clone());
+                if leval(state.get(0).clone(), scopes) == num!(1) {
+                    return leval(state.get(1).clone(), scopes);
                 }
             } else {
                 panic!("cond state error!")

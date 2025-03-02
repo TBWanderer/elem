59f90a5f55fe8123f500d005352b0d965d146474
TBWanderer <evgenypodyryaka@yandex.ru>
2025-03-02 11:30:22 +0300

    Fixed printing

--- src/libs/lio/functions.rs
+++ src/libs/lio/functions.rs
@@ -3,7 +3,11 @@ use crate::lang::{scopes::Scopes, value::*};
 pub fn lwrite(args: Value, _scopes: &mut Scopes) -> Value {
     use crate::nil;
 
-    print!("{}", args);
+    if args.len() > 1 {
+        panic!("Too many args for print func")
+    } else if args.len() == 1 {
+        print!("{}", args.get(0).show())
+    }
 
     nil!()
 }
@@ -11,7 +15,13 @@ pub fn lwrite(args: Value, _scopes: &mut Scopes) -> Value {
 pub fn lprint(args: Value, _scopes: &mut Scopes) -> Value {
     use crate::nil;
 
-    println!("{}", args);
+    if args.len() > 1 {
+        panic!("Too many args for print func")
+    } else if args.len() == 1 {
+        println!("{}", args.get(0).show())
+    } else {
+        println!()
+    }
 
     nil!()
 }

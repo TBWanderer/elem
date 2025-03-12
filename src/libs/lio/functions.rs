6953b71dd18aee33572724dd1a325f2838873af5
TBWanderer <evgenypodyryaka@yandex.ru>
2025-03-12 12:14:40 +0300

    Added returning Error type

--- src/libs/lio/functions.rs
+++ src/libs/lio/functions.rs
@@ -4,7 +4,9 @@ pub fn lwrite(args: Value, _scopes: &mut Scopes) -> Value {
     use crate::nil;
 
     if args.len() > 1 {
-        panic!("Too many args for print func")
+        return Value::Error(
+            "<func> write: Incorrect count of arguments! Expected less than 2 args".to_string(),
+        );
     } else if args.len() == 1 {
         print!("{}", args.get(0).show())
     }
@@ -16,12 +18,30 @@ pub fn lprint(args: Value, _scopes: &mut Scopes) -> Value {
     use crate::nil;
 
     if args.len() > 1 {
-        panic!("Too many args for print func")
+        return Value::Error(
+            "<func> print: Incorrect count of arguments! Expected less than 2 args".to_string(),
+        );
     } else if args.len() == 1 {
-        println!("{}", args.get(0).show())
+        let arg = args.get(0);
+        if let Value::Error(err) = arg {
+            return Value::Error(format!("<func> print: catched error in argument\n{}", err));
+        } else {
+            println!("{}", arg.show())
+        }
     } else {
         println!()
     }
 
     nil!()
+}
+
+pub fn lread(args: Value, _scopes: &mut Scopes) -> Value {
+    if args.len() != 0 {
+        return Value::Error(
+            "<func> read: Incorrect count of arguments! Expected 0 args".to_string(),
+        );
+    }
+
+    let input = crate::runtime::utils::io::input("");
+    Value::String(input)
 }

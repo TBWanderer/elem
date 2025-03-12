859b1ae23e91f20f32abce2288376944b2dd4003
TBWanderer <evgenypodyryaka@yandex.ru>
2025-03-12 22:46:41 +0300

    Added buitins func

--- src/libs/mod.rs
+++ src/libs/mod.rs
@@ -1,4 +1,9 @@
-pub mod lio;
+mod lio;
 pub mod lstd;
 
 type Library = crate::lang::scopes::Scope;
+
+use std::collections::HashMap;
+pub fn builtins() -> HashMap<String, Library> {
+    HashMap::from([("io".to_string(), lio::init())])
+}

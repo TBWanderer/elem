26b11dbac810af6c4e0dcc9dc5c1bad6dd91e169
TBWanderer <evgenypodyryaka@yandex.ru>
2025-03-12 22:48:15 +0300

    Fixed runtime for modules

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,18 @@ fn main() {
     let mut elem_runtime = runtime::Runtime::new();
 
     if let Some(path) = matches.get_one::<String>("file") {
+        let pathbuf = std::path::PathBuf::from(path);
+        if pathbuf.exists() && !pathbuf.is_file() {
+            println!("Path is not file!");
+            std::process::exit(1);
+        }
+        let pathbuf = std::path::PathBuf::from(std::fs::canonicalize(pathbuf).unwrap());
+
+        elem_runtime.run(&format!(
+            r#"(set __main_dir_path__ "{}")"#,
+            pathbuf.parent().unwrap().to_str().unwrap_or("/")
+        ));
+
         let code = runtime::utils::io::read_file(&path);
         elem_runtime.run(&code);
     } else {

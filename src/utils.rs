f21c3bf655555fc9052e92727bf0e69063de984f
TBWanderer <evgenypodyryaka@yandex.ru>
2025-03-16 17:36:18 +0300

    Added code_from_file func

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-pub use std::fs::read_to_string;
+pub use std::{fs::read_to_string, process};
 
 pub fn input(prompt: &str) -> String {
     use std::io::{self, BufRead, Write};
@@ -19,5 +19,29 @@ pub fn input(prompt: &str) -> String {
         Err(_) => {
             panic!()
         }
+    }
+}
+
+pub fn code_from_file(path: &dyn AsRef<std::path::Path>) -> String {
+    let path = path.as_ref();
+    if path.exists() {
+        if !path.is_dir() {
+            let data = std::fs::read_to_string(&path)
+                .expect(&format!("Couldn't read file: {}", path.to_str().unwrap()));
+
+            let processed_data = data
+                .lines()
+                .map(|line| line.split(';').collect::<Vec<&str>>()[0].trim())
+                .collect::<Vec<&str>>()
+                .join(" ");
+
+            processed_data
+        } else {
+            println!("{} is a directory, not file", path.to_str().unwrap());
+            process::exit(1);
+        }
+    } else {
+        println!("File not found: {}", path.to_str().unwrap());
+        process::exit(1);
     }
 }

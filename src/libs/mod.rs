a605c99fa6a73a1345a23bfb1789a2298c489e58
TBWanderer <evgenypodyryaka@yandex.ru>
2025-03-23 15:49:49 +0300

    Optimized libs

--- src/libs/mod.rs
+++ src/libs/mod.rs
@@ -1,10 +1,14 @@
+pub mod lfs;
 pub mod lio;
 pub mod lstd;
 
-use crate::lang::Value;
+use crate::lang::{error, Value};
 use std::collections::HashMap;
 
 pub fn builtins() -> HashMap<String, Value> {
     let key = |key: &str| key.to_string();
-    HashMap::from([(key("io"), lio::init())])
+    HashMap::from([
+        (key(lio::LIB_NAME), lio::init()),
+        (key(lfs::LIB_NAME), lfs::init()),
+    ])
 }

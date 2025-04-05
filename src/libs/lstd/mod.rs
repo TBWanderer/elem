637385eb4c676555eddf776b9a11991a885368c0
TBWanderer <evgenypodyryaka@yandex.ru>
2025-04-05 13:01:17 +0300

    Added new names

--- src/libs/lstd/mod.rs
+++ src/libs/lstd/mod.rs
@@ -13,12 +13,18 @@ pub fn init() -> Value {
     let fun = |lfun| Value::Function(lfun);
     let mac = |lmac| Value::Macro(lmac);
     Value::Struct(Struct::from([
+        (key("free"), mac(Rc::new(macros::lfree))),
         (key("pub"), mac(Rc::new(macros::lpub))),
         (key("set"), mac(Rc::new(macros::lset))),
+        (key("lambda"), mac(Rc::new(macros::llambda))),
         (key("eval"), mac(Rc::new(macros::leval))),
+        (key("evals"), fun(Rc::new(functions::levals))),
         (key("import"), fun(Rc::new(functions::limport))),
         (key("array"), fun(Rc::new(functions::llist))),
         (key("add"), fun(Rc::new(functions::ladd))),
         (key("mul"), fun(Rc::new(functions::lmul))),
+        // Adding bool names
+        (key("true"), Value::Bool(true)),
+        (key("false"), Value::Bool(false)),
     ]))
 }
